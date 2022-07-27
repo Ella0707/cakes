e1221dcb91ae81b3009f56c7d0cd36de15b9551a
Ella0707 <pakelvi77@gmail.com>
2022-07-27 19:56:01 +0300

    work

--- js/main.js
+++ js/main.js
@@ -1,29 +1,46 @@
 
 
 // мобильное меню
-// $('.header__burger').on('click', function (e) {
-//     e.preventDefault();
-//     document.body.classList.toggle('lock');
-//     $('.header__burger').toggleClass("active");
-//     $('.header__bottom').toggleClass("active");
-// });
+$('.profile-open').on('click', function (e) {
+  e.preventDefault();
+  document.body.classList.toggle('lock');
+  $('.profile-open').toggleClass("active");
+  $('.profile__info').toggleClass("active");
+});
+
+// сайдбар пользователя
+$('.header__burger-btn').on('click', function (e) {
+    e.preventDefault();
+    document.body.classList.toggle('lock');
+    $('.header__burger-btn').toggleClass("active");
+    $('.header__nav').toggleClass("active");
+});
 
 
 
 // слайдеры
-new Swiper('.profile__portfolio-slider', {
-    slidesPerView: 3.4,
-    observer: true,
-    observeParents: true,
-    loop: true,
-    centeredSlides: true,
 
-    navigation: {
-        nextEl: '.slider-arrow-next',
-        prevEl: '.slider-arrow-prev',
-    },
+const activitySlider = new Swiper('#destroy-1', {
+  slidesPerView: 3.4,
+  observer: true,
+  observeParents: true,
+  loop: true,
+  centeredSlides: true,
+
+  navigation: {
+      nextEl: '.slider-arrow-next',
+      prevEl: '.slider-arrow-prev',
+  },
 });
 
+if ($('#destroy-1').length) {
+  if ($(window).width() <= 769) {
+      activitySlider.destroy();
+  }
+}
+
+
+// табы в профиле
 function openTab(evt, tabsName) {
     var i, tabcontent, tablinks;
 
@@ -40,3 +57,27 @@ function openTab(evt, tabsName) {
     document.getElementById(tabsName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+
+// кнопка "наверх"
+var btn = $('.footer__up');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 100) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '100');
+});
+
+// класс active для ссылок в хэдере (навигация)
+$(".header__nav ul li").click(function (e) {
+    e.preventDefault();
+    $(".header__nav ul li").removeClass('active');
+    $(this).addClass('active');
+})
