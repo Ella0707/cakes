94f4f1c8536dd0e43c7fed1c6753c0c8fd901e37
unknown <pakelvi0707@gmail.com>
2022-08-04 15:58:23 +0300

    work

--- js/main.js
+++ js/main.js
@@ -148,3 +148,46 @@ $(".pagination__item").click(function (e) {
   $(".pagination__item").removeClass('active');
   $(this).addClass('active');
 })
+
+
+
+// выбор города с поиском на странице "мой профиль"
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options-container");
+const searchBox = document.querySelector(".search-box input");
+
+const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+    optionsContainer.classList.toggle("active");
+
+    searchBox.value = "";
+    filterList("");
+
+    if (optionsContainer.classList.contains("active")) {
+        searchBox.focus();
+    }
+});
+
+optionsList.forEach(o => {
+    o.addEventListener("click", () => {
+        selected.innerHTML = o.querySelector("label").innerHTML;
+        optionsContainer.classList.remove("active");
+    });
+});
+
+searchBox.addEventListener("keyup", function (e) {
+    filterList(e.target.value);
+});
+
+const filterList = searchTerm => {
+    searchTerm = searchTerm.toLowerCase();
+    optionsList.forEach(option => {
+        let label = option.firstElementChild.nextElementSibling.innerText.toLowerCase();
+        if (label.indexOf(searchTerm) != -1) {
+            option.style.display = "block";
+        } else {
+            option.style.display = "none";
+        }
+    });
+};
